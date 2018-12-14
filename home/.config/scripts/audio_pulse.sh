cfcf702cef6fcf1d86888c30b1871d4e9e6ddf17
koltrast <koltrast@pm.me>
2018-12-15 00:48:21 +0100

    sample rate to 48000Hz + various scripts update

--- home/.config/scripts/audio_pulse.sh
+++ home/.config/scripts/audio_pulse.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#This is the ffmpeg command that the audio shortcut in i3 will run.
-
 #Picks a file name for the output file based on availability:
-
 while [[ -f $HOME/audio$n.flac ]]
 do
 	n=$((n+1))
@@ -11,7 +8,6 @@ done
 filename="$HOME/audio$n.flac"
 
 #The actual ffmpeg command:
-
 ffmpeg \
 -f alsa -i default \
 -c:a flac \
--- home/.config/scripts/screencast_pulse.sh
+++ home/.config/scripts/screencast_pulse.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#This is the ffmpeg command that the screencast shortcut in i3 will run.
-
 #Picks a file name for the output file based on availability:
-
 while [[ -f $HOME/screencast$n.mkv ]]
 do
 	n=$((n+1))
@@ -11,7 +8,6 @@ done
 filename="$HOME/screencast$n.mkv"
 
 #The actual ffmpeg command:
-
 ffmpeg -y \
 -f x11grab \
 -s $(xdpyinfo | grep dimensions | awk '{print $2;}') \
